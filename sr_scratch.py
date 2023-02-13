0517a0836bc9559a1c0d93cfef266f32efbeafa3
Yujin Yano <northwindapps@gmail.com>
2023-02-13 23:17:22 +0900

    hanning window, fft

--- sr_scratch.py
+++ sr_scratch.py
@@ -4,7 +4,13 @@ import scipy.io
 import logging
 import sys
 import numpy
+from numpy.fft import fft, fftshift
 numpy.set_printoptions(threshold=sys.maxsize)
 
 samplerate, data = wavfile.read('./test.wav')
-print(data)
+# print(data)
+
+window = numpy.hanning(51)
+A = fft(window, 2048) / 25.5
+
+print(A)
